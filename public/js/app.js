d6e99c5891c466f4c79872d0abccb39a4e5218e8
Andrew Grosser <dioptre@gmail.com>
2014-10-04 18:55:20 +1000

    init ember

---
+++ public/js/app.js
@@ -0,0 +1,161 @@
+Ember.Application.initializer({
+  name: 'userapp',
+  initialize: function(container, application) {
+  	Ember.UserApp.setup(application, { appId: '542f6b2c8ea22' });
+  }
+});
+
+App = Ember.Application.create();
+
+var afterRenderEvent = function () {
+ 
+    UserApp.Token.heartbeat(function(error, result){
+		// Handle error/result
+		console.log('heartbeat');
+		//Todo transition to #login unless already there
+	});
+
+};
+Ember.View.reopen({
+  didInsertElement : function(){
+    this._super();
+    Ember.run.scheduleOnce('afterRender', App, afterRenderEvent);
+  }
+});
+
+App.Router.map(function() {
+  this.route('signup');
+  this.route('login');
+  this.route('article');
+  this.route('subscribe');
+});
+
+App.ApplicationRoute = Ember.Route.extend(Ember.UserApp.ApplicationRouteMixin);
+App.SignupController = Ember.Controller.extend(Ember.UserApp.FormControllerMixin);
+App.LoginController = Ember.Controller.extend(Ember.UserApp.FormControllerMixin);
+App.IndexRoute = Ember.Route.extend(Ember.UserApp.ProtectedRouteMixin);
+
+App.SubscribeRoute = Ember.Route.extend(Ember.UserApp.ProtectedRouteMixin);
+App.SubscribeView = Ember.View.extend({
+	didInsertElement: function () {
+		// this identifies your website in the createToken call below
+		Stripe.setPublishableKey('pk_test_eGxKrTUUvwyBCCwUjiwqXCBZ');
+
+		var stripeResponseHandler = function (status, response) {
+			if (response.error) {
+				// re-enable the submit button
+				$('.submit-button').removeAttr("disabled");
+				// show the errors on the form
+				$(".payment-errors").html(response.error.message);
+			} else {
+				UserApp.User.PaymentMethod.save({
+					user_id: 'self',
+					name: 'Stripe',
+					processor: 'stripe',
+					type: 'creditcard',
+					data: { token_id: response.id, amount: 250 }
+				}, function(error, result) {
+					// handle error/result
+				});
+				UserApp.User.save({
+					user_id: 'self',
+					subscription: {
+						price_list_id: '8OGiDhNcRiCmuQNcGkzZ0A',
+						plan_id: 'ebgLXRIrR3qDGu_frfNksA'
+					}
+				});
+				
+				var form$ = $("#payment-form");
+				// token contains id, last4, and card type
+				var token = response['id'];
+				// insert the token into the form so it gets submitted to the server
+				form$.append("<input type='hidden' name='stripeToken' value='" + token + "' />");
+				// and submit
+				form$.get(0).submit();
+			}
+		}
+
+		$(document).ready(function() {
+			$("#payment-form").submit(function(event) {
+				// disable the submit button to prevent repeated clicks
+				$('.submit-button').attr("disabled", "disabled");
+
+				// createToken returns immediately - the supplied callback submits the form if there are no errors
+				Stripe.createToken({
+					number: $('.card-number').val(),
+					cvc: $('.card-cvc').val(),
+					exp_month: $('.card-expiry-month').val(),
+					exp_year: $('.card-expiry-year').val()
+				}, stripeResponseHandler);
+				return false; // submit from callback
+			});
+		});      
+	},
+	actions: {
+		likesMusic: function () {
+			UserApp.User.save({"user_id": 'self', properties: { "likes_music": {"value": 1, "override": true }}			
+			}, function (error,result) {
+				//console.log(error, result);
+			});
+		}
+	}
+});
+
+App.ArticleController = Ember.ObjectController.extend({
+	// Title: function (key, value, previousValue) {
+		// // setter
+		// if (arguments.length > 1) {
+		  // var title = this.get('model');
+		  // title.set('title', value);
+		  // title.save();
+		// }
+
+		// // getter
+		// return this.get('model.title');
+	// }.property('model.title'),
+	// actions: {
+
+	// }
+});
+App.ArticleRoute = Ember.Route.extend(Ember.UserApp.ProtectedRouteMixin, {
+	beforeModel: function() {
+		// $.get('/articles.php', function(data) {
+			// App.articlesController.set('content', data);
+		// });
+	},
+	model: function() {
+		return  this.store.findAll('article');
+	}
+});
+
+DS.RESTAdapter.reopen({
+  buildURL: function(klass, id) {
+  if (id)
+	return '/' + klass + 's.php?id=' + id;
+	else
+	return '/' + klass + 's.php';
+    // var urlRoot = Ember.get(klass, 'url');
+    // if (!urlRoot) { throw new Error('Ember.RESTAdapter requires a `url` property to be specified'); }
+
+    // if (!Ember.isEmpty(id)) {
+      // return urlRoot + "/" + id;
+    // } else {
+      // return urlRoot;
+    // }
+  }
+});
+
+// App.ApplicationAdapter = DS.RESTAdapter.extend({
+    // host: 'http://verve.dev'
+// });
+
+// App.ApplicationSerializer = DS.RESTSerializer.extend({
+    // primaryKey: 'id',
+    // serializeId: function(id) {
+        // return id.toString();
+    // }
+// });
+
+App.Article = DS.Model.extend({
+  title: DS.attr('string')
+});
---
+++ subscribe.php
@@ -0,0 +1,91 @@
+<?php
+	error_reporting(E_ALL);
+	ini_set('display_errors', '1');	
+
+	use \UserApp\Widget\User;
+	require("app_init.php");
+	
+
+	$valid_token = false;
+
+	if(isset($_COOKIE["ua_session_token"])){
+		$token = $_COOKIE["ua_session_token"];
+
+		try{
+			$valid_token = User::loginWithToken($token);
+			$user = User::current();
+		}catch(\UserApp\Exceptions\ServiceException $exception){
+			$valid_token = false;
+		}
+	}
+	//echo (string)serialize($user->properties->stripe_id);
+	if(!$valid_token or !isset($_POST['stripeToken'])){
+		echo "Invalid token";
+	}else {
+		// Set your secret key: remember to change this to your live secret key in production
+		// See your keys here https://dashboard.stripe.com/account
+		Stripe::setApiKey("sk_test_VnTLRckG5HMoF2o9ZOEUeuV6");
+		$stoken = $_POST['stripeToken'];
+
+		$sid = null;
+		
+		$api = new \UserApp\API($GLOBALS['userAppId'], "qKXq1OUfSQaB3GZiGGNtuw");
+
+		$result = $api->user->get(array(
+			"user_id" => $user->user_id
+		));
+		if (isset($result[0]->properties->stripe_id->value)) {
+			$sid = $result[0]->properties->stripe_id->value;
+		}
+		else {
+			// Get the credit card details submitted by the form
+			$customer = Stripe_Customer::create(array(
+			  'email' => $user->email,
+			  'card'  => $stoken
+			));
+			$sid = $customer->id;	
+			$result = $api->user->save(array(
+				'user_id' => $user->user_id,
+				'properties' => array( 'stripe_id' => array ('value' => $sid, 'override' => true))
+			));
+		}
+		
+		
+		// $result = $api->property->save(array(
+			// //"property_id" => $user->user_id . "-stripe",
+			// "name" => "stripe_id",
+			// "type" => "string",
+			// "default_value" => "",
+			// "access_level" => "private"
+		// ));
+		
+		
+		// $result = $api->property->search(array(
+    // "page" => 1,
+    // "page_size" => 100,
+    // "fields" => "*",
+    // "sort" => "asc"
+// ));
+	// echo (string)serialize($result);
+	
+	// $result = $api->property->remove(array(
+    // "property_id" => "cMtn9g4KE8aQd8QFElhlFR"
+// ));
+
+		$charge = Stripe_Charge::create(array(
+		  'customer' => $sid,
+		  'amount'   => 25000,
+		  'currency' => 'aud'
+		));
+
+		
+		// $customer = Stripe_Customer::create(array(
+		  // "card" => $stoken,
+		  // "plan" => "VERVE1",
+		  // "email" => "payinguser@example.com")
+		// );
+
+		echo '<h1>Successfully charged AUD $250.00!</h1>';
+
+	}
+?>
