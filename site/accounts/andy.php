e8326493c7f6cb2c50b1167ba3c56a0344c7856c
Andrew Grosser <dioptre@gmail.com>
2014-10-05 12:31:52 +1000

    added my account and hosting requirements iusr 'iis appool\verve'

---
+++ site/accounts/andy.php
@@ -0,0 +1,19 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: andy
+email: andrew@expedit.com.au
+password: >
+  $2a$10$OeUr2mJhCzpeI2gElE3sluYDPrT7lwl4oJkeP57sfFC50ywrJQC0m
+language: en
+role: admin
+token: d7ef8f9a1d59f6a51efa45ae496c3565c31dc3cb
+history:
+  - home/ready-to-buy
+  - home/testimonials
+  - null
+  - home
+  - home/how-does-it-work/experience-amazing
+firstname:
+lastname:
+firstName:
+lastName:
