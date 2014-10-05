3c2e03b7f75ec58efb76ad488e18604633d2c6e4
Andrew Grosser <dioptre@gmail.com>
2014-10-05 13:00:11 +1000

    added users

--- site/accounts/andy.php
+++ site/accounts/andy.php
@@ -3,17 +3,5 @@
 username: andy
 email: andrew@expedit.com.au
 password: >
-  $2a$10$OeUr2mJhCzpeI2gElE3sluYDPrT7lwl4oJkeP57sfFC50ywrJQC0m
+  $2a$10$zl/uU6EjztdFuYmrpm/soOSleVyijyb1VTwl.4L/YiZfl8wSe9F4O
 language: en
-role: admin
-token: d7ef8f9a1d59f6a51efa45ae496c3565c31dc3cb
-history:
-  - home/ready-to-buy
-  - home/testimonials
-  - null
-  - home
-  - home/how-does-it-work/experience-amazing
-firstname:
-lastname:
-firstName:
-lastName:
