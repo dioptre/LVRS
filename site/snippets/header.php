0c99cd1c586d3b390bc4c8ffd00efcd57e9c9fd2
Paul Knittel <paulknittel@live.com>
2014-10-21 15:26:43 -0700

    james feedback.. google analytics and csss

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -31,6 +31,10 @@
       "assets/css/custom-verve.css"
     )); ?>
 
+    <style>
+      <?php echo html($site->css()); ?>
+    </style>
+
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -57,31 +61,37 @@
 
 
     <script>
-      // mixpanel
-      (function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
-      for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="//cdn.mxpnl.com/libs/mixpanel-2.2.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
+    // MIXPANEL NOT SETUP YET
+    // mixpanel
+    // (function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
+    // for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="//cdn.mxpnl.com/libs/mixpanel-2.2.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
 
-      // google analytics
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    // google analytics
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 
-      if (window.location.host === "flowpro.io"){
-        ga('create', 'UA-51648279-1', 'flowpro.io'); // use the real one
-        mixpanel.init("8dff7dce3b6e6f7ab2e1acc9815fb2ca"); // mixpanel dev
-      } else {
-        ga('create', 'UA-51648279-4', 'flowpro.dev'); // use the dev one
-        mixpanel.init("0672707ed6141205517c7514f9b2337b"); // mixpanel dev
-      }
+    if (window.location.host === "lvrs.co"){
+      // LVRS shared google anlytics
+      ga('create', 'UA-55964340-1', 'auto');
 
-      // mixpanel track event
-      mixpanel.track('Page Viewed', {
-          pageName: document.title,
-          domain: location.host,
-          url: location.href
-      });
+      // Mixpanel
+      //mixpanel.init("8dff7dce3b6e6f7ab2e1acc9815fb2ca"); // mixpanel dev
+    } else {
+
+      // Should setup a dev one as well
+      ga('create', 'UA-55964340-2', 'auto'); // use the dev one
+      //mixpanel.init("0672707ed6141205517c7514f9b2337b"); // mixpanel dev
+    }
+
+    // mixpanel track event
+    // mixpanel.track('Page Viewed', {
+    //     pageName: document.title,
+    //     domain: location.host,
+    //     url: location.href
+    // });
 
       // google track event
       ga('require', 'displayfeatures');
