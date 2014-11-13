fec440b6da36354d76d3fc99dd96e56f488d2eff
Paul Knittel <paulknittel@live.com>
2014-11-12 17:41:18 -0800

    Updated panel

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -63,6 +63,8 @@
       "assets/js/main.js"
     )); ?>
 
+    <script src="//cdn.optimizely.com/js/2202300041.js"></script>
+
     <script>
       // Include the UserVoice JavaScript SDK (only needed once on a page)
       UserVoice=window.UserVoice||[];(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/qkIZqSkIfHpTfdIXEBzWTA.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
